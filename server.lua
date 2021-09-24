99d6328883afb1ad54cd4e89ac620bb90d4ba415
Desperados <89983547+Desperados-Creation@users.noreply.github.com>
2021-09-24 20:17:04 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -8,23 +8,16 @@ AddEventHandler('esx:playerLoaded', function(xPlayer)
 end)
 
 RegisterServerEvent('dpr_Shop:Achat')
-AddEventHandler('dpr_Shop:Achat', function(abcdef, Label, Value, Price) 
+AddEventHandler('dpr_Shop:Achat', function(Label, Value, Price) 
+	local _src = source
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer.getMoney() >= Price then
 	    xPlayer.removeMoney(Price)
     	xPlayer.addInventoryItem(Value, 1)
-		local xPlayers	= ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			Citizen.Wait(500) 
-			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Supérette', '~o~2~r~4~g~7', "Vous venez d\'acheter ~b~1x "..Label.." ~s~pour ~r~"..Price.."$ ~s~!", 'CHAR_ACTING_UP', 1)
-		end
+		Citizen.Wait(500) 
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Supérette', '~o~2~r~4~g~7', "Vous venez d\'acheter ~b~1x "..Label.." ~s~pour ~r~"..Price.."$ ~s~!", 'CHAR_ACTING_UP', 1)
 	else 
-		local xPlayers	= ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			Citizen.Wait(500) 
-        	TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Supérette', '~o~2~r~4~g~7', 'Vous n\'avez pas suffisament d\'argent !', 'CHAR_ACTING_UP', 1)
-		end
+		Citizen.Wait(500) 
+    	TriggerClientEvent('esx:showAdvancedNotification', _src, 'Supérette', '~o~2~r~4~g~7', 'Vous n\'avez pas suffisament d\'argent !', 'CHAR_ACTING_UP', 1)
     end
 end)  
